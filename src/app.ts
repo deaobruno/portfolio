d527b30528d086d362ec0f7eb0f2e322fdefb540
Bruno Oliveira <bruno.deao@gmail.com>
2024-09-22 12:50:27 -0300

    async startup

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,7 @@ import config from './config'
 import server from './server'
 import db from './db'
 
-db.start(config.db.mongo.url)
-server.start(config.api.http.port)
+(async () => {
+  await db.start(config.db.mongo.url)
+  server.start(config.api.http.port)
+})()
