8125ebd024eb0623ef1da8375dd8c3b18393b9d6
Bruno Oliveira <bruno.deao@gmail.com>
2024-09-21 23:49:29 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,31 @@
 import { Router } from 'express'
 
 export default (router: Router) => {
-  router.get('/', (req, res) => {
-    res.send('Hello world')
+  router.post('/auth/login', (req, res) => {
+    res.send('login')
+  })
+
+  router.post('/auth/logout', (req, res) => {
+    res.send('logout')
+  })
+
+  router.post('/projects', (req, res) => {
+    res.send('create project')
+  })
+
+  router.get('/projects', (req, res) => {
+    res.send('list projects')
+  })
+
+  router.get('/projects/:project_id', (req, res) => {
+    res.send('get project by id')
+  })
+
+  router.put('/projects/project_id', (req, res) => {
+    res.send('update project')
+  })
+
+  router.delete('/projects/:project_id', (req, res) => {
+    res.send('delete project')
   })
 }
