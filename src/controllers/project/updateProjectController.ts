8d24addf03b9470c3836f7455100b0ebfc4a0eb5
Bruno Oliveira <bruno.deao@gmail.com>
2024-09-27 17:32:12 -0300

    validate project exists before update & delete

--- src/controllers/project/updateProjectController.ts
+++ src/controllers/project/updateProjectController.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import Project from '../../models/project/Project'
+import NotFoundError from '../../errors/NotFoundError'
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { project_id } = req.params
+    const project = await Project.findOne({ project_id })
+
+    if (!project) return next(NotFoundError('Project not found'))
+
     const { name, description, url, repository } = req.body
     const cover = req.file && req.file.path.replace('public', '')
 
