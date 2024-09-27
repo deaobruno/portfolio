094a592cdc2a04e199e7dba8b15a02c0dbfb9373
Bruno Oliveira <bruno.deao@gmail.com>
2024-09-27 17:34:28 -0300

    remove project cover on project delete

--- src/controllers/project/deleteProjectController.ts
+++ src/controllers/project/deleteProjectController.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path'
+import { promises as fs } from 'node:fs'
 import { NextFunction, Request, Response } from 'express'
 import Project from '../../models/project/Project'
 import NotFoundError from '../../errors/NotFoundError'
@@ -8,6 +10,8 @@ export default async (req: Request, res: Response, next: NextFunction) => {
     const project = await Project.findOne({ project_id })
 
     if (!project) return next(NotFoundError('Project not found'))
+    if (project.cover && project.cover !== '/images/default-project.jpeg')
+      await fs.unlink(resolve(`./public${project.cover}`))
 
     await Project.deleteOne({ project_id })
 
