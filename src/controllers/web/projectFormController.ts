f34506f790d8d9fc862624ba01f6b835667846be
Bruno Oliveira <bruno.deao@gmail.com>
2024-10-13 11:59:10 -0300

    projectFormController validations

--- src/controllers/web/projectFormController.ts
+++ src/controllers/web/projectFormController.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import Project from '../../models/project/Project'
+import NotFoundError from '../../errors/NotFoundError'
 
-export default async (req: Request, res: Response) => {
+export default async (req: Request, res: Response, next: NextFunction) => {
   const { project_id } = req.params
+  const defaultCover = '/images/default-project.jpeg'
 
-  if (!project_id) return res.render('project-form.html')
+  if (!project_id)
+    return res.render('project-form.html', { project: { cover: defaultCover }})
 
   const project = await Project.findOne({ project_id })
+
+  if (!project) return next(NotFoundError('Project not found'))
+  if (!project.cover) project.cover = defaultCover
 
   res.render('project-form.html', { project })
 }
