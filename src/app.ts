cfa78b35cff97b200530c7e9f77e826727b5b8a4
Bruno Oliveira <bruno.deao@gmail.com>
2024-10-13 11:38:47 -0300

    log error on app startup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import config from './config'
+import logger from './drivers/logger'
 import server from './drivers/server'
 import db from './drivers/db'
 
 ;(async () => {
-  await db.start(config.db.mongo.url)
-  server.start(config.http.port)
+  try {
+    await db.start(config.db.mongo.url)
+    server.start(config.http.port)
+  } catch (error: any) {
+    logger.debug(error)
+    logger.fatal(error.message)
+  }
 })()
